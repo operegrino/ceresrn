5f5c7945aeba258475e267306b0b5188a38c2bfd
carvalho0201@gmail.com <carvalho0201@gmail.com@48c05b85-636d-7a96-97a5-1fcfb743fa5a>
2013-06-13 17:39:25 +0000

    Notas da versão: Desenvolvido funcionalidade completa de login! Adição de backup da base de dados. Usuário: carvalho0201, senha: 030824duke , base: ceres

--- trunk/unit/uLogin.pas
+++ trunk/unit/uLogin.pas
@@ -5,10 +5,14 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Buttons,
-  Vcl.Imaging.pngimage, Vcl.ExtCtrls;
+  Vcl.Imaging.pngimage, Vcl.ExtCtrls, uADStanIntf, uADStanOption, uADStanError,
+  uADGUIxIntf, uADPhysIntf, uADStanDef, uADStanPool, uADStanAsync,
+  uADPhysManager, uADStanParam, uADDatSManager, uADDAptIntf, uADDAptManager,
+  uADGUIxFormsWait, uADPhysODBCBase, uADPhysMSSQL, uADCompGUIx, Data.DB,
+  uADCompDataSet, uADCompClient;
 
 type
-  TForm1 = class(TForm)
+  TfrmLogin = class(TForm)
     Image1: TImage;
     GroupBox1: TGroupBox;
     edLogin: TEdit;
@@ -16,7 +20,12 @@ type
     Label2: TLabel;
     edSenha: TEdit;
     BitBtn1: TBitBtn;
+    ADConnection1: TADConnection;
+    Qr_Login: TADQuery;
+    ADGUIxWaitCursor1: TADGUIxWaitCursor;
+    ADPhysMSSQLDriverLink1: TADPhysMSSQLDriverLink;
     procedure Image1Click(Sender: TObject);
+    procedure BitBtn1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -24,13 +33,67 @@ type
   end;
 
 var
-  Form1: TForm1;
+  frmLogin: TfrmLogin;
 
 implementation
 
 {$R *.dfm}
 
-procedure TForm1.Image1Click(Sender: TObject);
+uses uPrincipal;
+
+procedure TfrmLogin.BitBtn1Click(Sender: TObject);
+var
+  login, senha : String;
+begin
+
+  login:= TRIM(edLogin.Text);
+  senha:= TRIM(edSenha.Text);
+
+  if login ='' then
+  begin
+    MessageDlg('Favor informe o login!', mtInformation,[mbOK],0);
+    edLogin.Clear;
+    edLogin.SetFocus;
+    EXIT;
+  end;
+
+  if senha='' then
+  begin
+    MessageDlg('Favor informe a senha!', mtInformation,[mbOK],0);
+    edSenha.Clear;
+    edSenha.SetFocus;
+    EXIT;
+  end;
+
+  Qr_Login.Close;
+  Qr_Login.SQL.Clear;
+  Qr_Login.SQL.Add('SELECT * FROM usuario');
+  Qr_Login.SQL.Add('WHERE login = :P_param1');
+  Qr_Login.SQL.Add('AND senha = :P_param2');
+
+  Qr_Login.ParamByName('p_param1').AsString:=login;
+  Qr_Login.ParamByName('p_param2').AsString:=senha;
+  Qr_Login.Open();
+
+  if Qr_Login.IsEmpty then
+  begin
+    MessageDlg('Combinação Login/Senha inválida! Tente novamente!',mtError,[mbOk],0);
+    edLogin.Clear;
+    edSenha.Clear;
+    login:='';
+    senha:='';
+    edLogin.SetFocus;
+    EXIT;
+  end;
+
+  frmLogin.Visible      :=FALSE;
+  frmPrincipal.Visible  :=TRUE;
+
+
+
+end;
+
+procedure TfrmLogin.Image1Click(Sender: TObject);
 begin
   MessageDlg('Propriedade Oak Systems | Cópia licenciada para Natália Oliveira',mtInformation,[mbOK],0);
 end;
